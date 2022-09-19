3214df7b88d9e8e5cf08e850b3695fbdef1c1500
Luiz Alberto Albuquerque <104793519+luizAlbuquerque0@users.noreply.github.com>
2022-09-19 15:57:39 -0300

    Implement DbContext

---
+++ RecordShop.Infrastructure/Persistence/RecordShopDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordShop.Infrastructure.Persistence
+{
+    public class RecordShopDbContext : DbContext
+    {
+        public RecordShopDbContext(DbContextOptions<RecordShopDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
